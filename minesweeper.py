3a7c888f53075fafc41d1587b1eb9a7a583700aa
kennedyry <64114263+kennedyry@users.noreply.github.com>
2021-06-23 17:17:45 -0400

    Added final code for project
    Refactored and cleaned up methods.

--- minesweeper.py
+++ minesweeper.py
@@ -2,6 +2,13 @@ import numpy as np
 import random as rand
 import copy
 
+LEARNING_RATE = .6
+
+EXPLORE_PROB = 0.01
+DISCOUNT_FACTOR = 0.8
+LEARNING_RATE = 0.01
+MAX_MOVES = 1000
+
 '''
 '''
 
@@ -31,21 +38,12 @@ class Minesweeper:
 
         self.game_over = False
 
-        # Uncomment this to have all cells revealed.
-        # self.user_board = np.array([np.ones(width)] * height)
-
         self.bombs = bombs
         self.revealed_cells = 0
 
         self.generate_bombs()
         self.set_neighboring_bombs()
 
-        self.BOMB_REWARD = -250
-        self.EXPLORE_PROB = 0.01
-        self.DISCOUNT_FACTOR = 0.8
-        self.LEARNING_RATE = 0.01
-
-        self.MAX_MOVES = 1000
 
     def restart(self):
         return Minesweeper(len(self.sol_board[0]), len(self.sol_board), self.bombs)
@@ -183,31 +181,51 @@ class Minesweeper:
                         out.add((row, col))
         return list(out)
 
-    def expected_val_for_cell(self, row, col):
+    def expected_val_for_cell(self, row, col, total_type):
         '''
         Calculates the expected state for the cell - being the total sum of all revealed neighboring cells.
         Returns negative because having a large number would technically be bad.
         :param row: The row of the cell
         :param col:  The col of the cell
+        :param total_type:  Whether we are returning the utility of a cell as the summed
+                            amount of total revealed bomb counts of neighboring cells
+                            or as a 3x3 grid of values representing each revealed neighboring
+                            cells neighboring bomb count or ' ' if not revealed.
         :return: The expected state of the cell
         '''
-        out = 0
+        sum_revealed_bombs = 0
         non_revealed = 0
         revealed = 0
         vals = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+        surrounding_states = [[' ', ' ', ' '],
+                              [' ', ' ', ' '],
+                              [' ', ' ', ' ']]
+
         for val in vals:
             r, c = val
 
             upd_r = row + r
             upd_c = col + c
 
+
             if self.is_revealed(upd_r, upd_c):
-                out += self.sol_board[upd_r][upd_c]
+                sum_revealed_bombs += self.sol_board[upd_r][upd_c]
+                surrounding_states[r + 1][c + 1] = self.sol_board[upd_r][upd_c]
                 revealed += 1
             else:
                 non_revealed += 1
 
-        return out, non_revealed
+
+        s_states_out = (tuple(surrounding_states[0]),
+                           tuple(surrounding_states[1]),
+                           tuple(surrounding_states[2]))
+
+        if total_type:
+            return sum_revealed_bombs, non_revealed
+        else:
+            return s_states_out, non_revealed
+
 
     def is_revealed(self, row, col):
         '''
@@ -243,14 +261,19 @@ class Minesweeper:
         print(self)
 
 
-class Policy:
-
-    def __init(self, problem):
-        self.best_actions = copy.deepcopy(problem)
-
-
-def q_solve(problem, iterations):
-    pass
+def q_solve(problem, iterations, total_type):
+    '''
+    Represents our training program, which given a game of minesweeper and the number of
+    iterations to train for, returns the appropriate q-values for that game.
+    :param problem: The game of minesweeper to train on - i.e. What configuration of the board
+    :param iterations: The number of iterations / games to play for training
+    :param total_type:  Whether we are returning the utility of a cell as the summed
+                            amount of total revealed bomb counts of neighboring cells
+                            or as a 3x3 grid of values representing each revealed neighboring
+                            cells neighboring bomb count or ' ' if not revealed.
+    :return: Q-Values corresponding to the optimal / bad moves to make given possible states.
+    '''
+    print("Starting Reinforcement Learning with ", iterations, " Iterations")
     problem = problem.restart()
 
     q_vals = {}
@@ -261,7 +284,7 @@ def q_solve(problem, iterations):
 
     for c_iter in range(1, iterations + 1):
         if (c_iter % 1000) == 0:
-            print("Current Iteration: ", c_iter, " Mean length of game: ", int(mean_length_of_game / 1000),
+            print("Current Iteration: ", c_iter, " Mean length of game: ", int(mean_length_of_game / (games_won + games_lost)),
                   " Games Lost: ", games_lost, " Games Won: ", games_won)
             mean_length_of_game = 0
             games_won = 0
@@ -276,10 +299,10 @@ def q_solve(problem, iterations):
         problem.reveal(current_row, current_col)
         if problem.is_game_over():
             continue
-        for c_move in range(problem.MAX_MOVES):
-            done, r, c = q_update(current_row, current_col, q_vals, problem)
+        for c_move in range(MAX_MOVES):
+            done, r, c = q_update(current_row, current_col, q_vals, problem, total_type)
             if done:
-                mean_length_of_game += c_move
+                mean_length_of_game += c_move + 1
                 break
             else:
                 current_row, current_col = (r, c)
@@ -287,35 +310,42 @@ def q_solve(problem, iterations):
             games_lost += 1
         else:
             games_won += 1
-    print("Done :)")
-    for key in q_vals:
-        print("Key : ", key, " - ", q_vals[key])
-
+    print("Finished Reinforcement Learning")
     return q_vals
 
 
-def q_update(row, col, q_vals, problem, policy=None):
+def q_update(row, col, q_vals, problem, total_type):
+    '''
+    Method that selectes each move to make given the current state of the game
+    and updates the q-values corresponding to the move made.
+    :param row: The row of the move previously made
+    :param col: The col of the move previously made
+    :param q_vals: The current dictionary of q-values
+    :param problem: The state of the game / the current minesweeper game.
+    :param total_type:  Whether we are returning the utility of a cell as the summed
+                            amount of total revealed bomb counts of neighboring cells
+                            or as a 3x3 grid of values representing each revealed neighboring
+                            cells neighboring bomb count or ' ' if not revealed.
+    :return: Whether the game is over (T/F) and the row and col of the move currently made.
+    '''
     chosen_move = None
     util_of_move = None
 
-    # Need some way to get all possible moves -> Convert into Move, Val pairs
 
     poss_moves = problem.possible_states()
-
-    # problem.expected_val_for_cell(row, col) -> Gets you bomb val for all revealed cells around it
 
     bomb_hidden_pairs = {}
 
     for move in poss_moves:
         r, c = move
-        neighboring_bomb_val, hidden_neighbors = problem.expected_val_for_cell(r, c)
+        neighboring_bomb_val, hidden_neighbors = problem.expected_val_for_cell(r, c, total_type)
 
         if not (neighboring_bomb_val, hidden_neighbors) in bomb_hidden_pairs:
             bomb_hidden_pairs[(neighboring_bomb_val, hidden_neighbors)] = move
 
-    if rand.random() < problem.EXPLORE_PROB:
+    if rand.random() < EXPLORE_PROB:
         chosen_move = poss_moves[rand.randrange(0, len(poss_moves))]
-        util_of_move = problem.expected_val_for_cell(chosen_move[0], chosen_move[1])
+        util_of_move = problem.expected_val_for_cell(chosen_move[0], chosen_move[1], total_type)
     else:
         chosen_move = None
         best_score = -float("inf")
@@ -344,18 +374,92 @@ def q_update(row, col, q_vals, problem, policy=None):
         # Probably isnt a good thing here, but how do we reward it not being a bomb
         q_val_actual = 10
 
-    q_vals[util_of_move] += problem.LEARNING_RATE * ((problem.DISCOUNT_FACTOR * q_val_actual) - q_val_predicted)
+    q_vals[util_of_move] += LEARNING_RATE * ((DISCOUNT_FACTOR * q_val_actual) - q_val_predicted)
     if problem.is_game_over():
         return True, 0, 0
     return False, chosen_move[0], chosen_move[1]
 
 
-if __name__ == "__main__":
-    game = Minesweeper(9, 9, 10)
+def test_set_accuracy(q_vals, game, iterations, total_type):
+    '''
+    Does basically what Q_Update is doing except that it does not actually update Q-Values
+    or use explore_probability to potentially make random moves - used purely to
+    see how many wins / losses it gets solely making decisions based on the values
+    of a corresponding move and its q-values.
+    :param q_vals: The Q-Values that have been created from our reinforcement model
+    :param game: The configuration of the game in which we are playing.
+    :param iterations: The number of games to simulate.
+    :param total_type:  Whether we are returning the utility of a cell as the summed
+                            amount of total revealed bomb counts of neighboring cells
+                            or as a 3x3 grid of values representing each revealed neighboring
+                            cells neighboring bomb count or ' ' if not revealed.
+    '''
+    print("Simulating ", iterations,
+          " games without explore probability to test win-rate without forced random moves")
 
-    q_vals = q_solve(game, 10000)
+    games_won = 0
+    games_lost = 0
+    for c_idx in range(iterations):
+        game = game.restart()
+        row = rand.randrange(0, len(game.sol_board))
+        col = rand.randrange(0, len(game.sol_board[0]))
 
+        game.reveal(row, col)
 
+        while game.is_game_over():
+            game = game.restart()
+            rand.seed()
+            row = rand.randrange(0, len(game.sol_board))
+            col = rand.randrange(0, len(game.sol_board[0]))
+
+            game.reveal(row, col)
+
+        while (not game.is_game_over()):
+            chosen_move = None
+            util_of_move = None
+
+            poss_moves = game.possible_states()
+
+            bomb_hidden_pairs = {}
+
+            for move in poss_moves:
+                r, c = move
+                neighboring_bomb_val, hidden_neighbors = game.expected_val_for_cell(r, c, total_type)
+
+                if not (neighboring_bomb_val, hidden_neighbors) in bomb_hidden_pairs:
+                    bomb_hidden_pairs[(neighboring_bomb_val, hidden_neighbors)] = move
+
+            chosen_move = None
+            best_score = -float("inf")
+
+            for key in bomb_hidden_pairs:
+                if key not in q_vals:
+                    q_vals[key] = 0
+                if chosen_move is None or q_vals[key] > best_score:
+                    chosen_move = bomb_hidden_pairs[key]
+                    best_score = q_vals[key]
+                    util_of_move = key
+            if util_of_move not in q_vals:
+                q_vals[util_of_move] = 0
+            game.reveal(chosen_move[0], chosen_move[1])
+        if game.game_over:
+            games_lost += 1
+        else:
+            games_won += 1
+    print("Simulated ", iterations, " Games lost: ", games_lost, " Games Won: ", games_won,
+          "Win Rate: ", games_won / (games_lost + games_won))
+
+def simulate_single_game(q_vals, game, total_type):
+    '''
+    Simulates a single game, where a user can play through a game seeing what move the
+    agent is making every time based on the q-values that we have gotten from training
+    :param q_vals: The q-values gained from training our agent.
+    :param game: The game configuration.
+    :param total_type:  Whether we are returning the utility of a cell as the summed
+                            amount of total revealed bomb counts of neighboring cells
+                            or as a 3x3 grid of values representing each revealed neighboring
+                            cells neighboring bomb count or ' ' if not revealed.
+    '''
     game = game.restart()
     row = rand.randrange(0, len(game.sol_board))
     col = rand.randrange(0, len(game.sol_board[0]))
@@ -370,32 +474,26 @@ if __name__ == "__main__":
 
         game.reveal(row, col)
 
-    while(not game.is_game_over()):
+    while (not game.is_game_over()):
         input("Continue?")
         print(game.__str__())
 
         chosen_move = None
         util_of_move = None
 
-        # Need some way to get all possible moves -> Convert into Move, Val pairs
-
         poss_moves = game.possible_states()
-
-        # problem.expected_val_for_cell(row, col) -> Gets you bomb val for all revealed cells around it
-
         bomb_hidden_pairs = {}
 
         for move in poss_moves:
             r, c = move
-            neighboring_bomb_val, hidden_neighbors = game.expected_val_for_cell(r, c)
+            neighboring_bomb_val, hidden_neighbors = game.expected_val_for_cell(r, c, total_type)
 
             if not (neighboring_bomb_val, hidden_neighbors) in bomb_hidden_pairs:
                 bomb_hidden_pairs[(neighboring_bomb_val, hidden_neighbors)] = move
 
-
         if 1 == 1:
             chosen_move = None
-            best_score = -10000
+            best_score = -float("inf")
 
             for key in bomb_hidden_pairs:
                 if key not in q_vals:
@@ -406,7 +504,6 @@ if __name__ == "__main__":
                     util_of_move = key
         if util_of_move not in q_vals:
             q_vals[util_of_move] = 0
-        q_val_predicted = q_vals[util_of_move]
 
         game.reveal(chosen_move[0], chosen_move[1])
     if game.game_over:
@@ -414,7 +511,17 @@ if __name__ == "__main__":
     else:
         print("You won!")
 
+if __name__ == "__main__":
+    game = Minesweeper(9, 9, 10)
+    q_vals = q_solve(game, 5000, False)
 
+    #If you want to step through seeing what decisions the agent makes every time run this
+    # simulate_single_game(q_vals, game)
+
+    #If you want to see the accuracy of it playing games without making random moves to train
+    #Run this.
+    for i in range(10):
+        test_set_accuracy(q_vals, game, 2000, False)
 
 
 
